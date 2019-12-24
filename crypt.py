070f4db65cb49568c1fa950ca98f9adaf1d11c89
Nan Cao <caonan93@gmail.com>
2019-12-25 01:33:37 +0800

    up

--- crypt.py
+++ crypt.py
@@ -11,28 +11,27 @@ import io
 from binascii import b2a_hex, a2b_hex
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding='utf-8', line_buffering=True)
 
-def Encrypt(key:str, text:str) -> str:
+def Encrypt(key:str, text:str):
     if len(key) < 32: key += ' ' * (32 - len(key))
     elif len(key) > 32: key = key[0:32]
-
-    if len(text) < 32: text += ' ' * (32 - len(text))
-    elif len(text) > 32: text = text[0:32]
+    btxt = base64.b64encode(text.encode('utf-8'))
+    if len(btxt) < 32: btxt += b' ' * (32 - len(btxt))
+    elif len(btxt) > 32: btxt = btxt[0:32]
     cipher = AES.new(bytes(key,encoding='utf-8'), AES.MODE_CBC, bytes(AES.block_size))
-    return str(b2a_hex(gzip.compress(bytes.strip(cipher.encrypt(text)))), encoding='utf-8')
+    print(btxt)
+    return gzip.compress(bytes.strip(cipher.encrypt(btxt)))
 
 def Decrypt(key:str, text:str) -> str:
     if len(key) < 32:
         key += ' ' * (32 - len(key))
     elif len(key) > 32:
         key = key[0:32]
-
-    btext = a2b_hex(text)
     cipher = AES.new(bytes(key,encoding='utf-8'), AES.MODE_CBC, bytes(AES.block_size))
-    return str(gzip.decompress(bytes.strip(cipher.decrypt(btext))), encoding='utf-8')
+    return str(gzip.decompress(bytes.strip(cipher.decrypt(base64.b64decode(text)))), encoding='utf-8')
 
-cipher_text = Encrypt("123","aaaaaa")
+cipher_text = Encrypt("123","hulkcao")
 
-print('encrypt:'+cipher_text)
+#print('encrypt:'+cipher_text)
 
-ret = Decrypt("123",cipher_text)
-print("ret:",ret)
+#ret = Decrypt("123",cipher_text)
+#print("ret:",ret)
